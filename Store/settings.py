4790d9ac160d450b3872833568236bfd3d7ac5f6
RozaMia <6007618@mail.ru>
2025-09-24 00:16:32 +0300

    Создали модели и провели миграции

--- Store/settings.py
+++ Store/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y7dxokii4e+dh@*e4@gq8wj!i446we0xxj25*m(i)hd&@1yuz='
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -46,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djmoney',
     'appModels',
     'appRequests',
     'appSettings'
@@ -127,12 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- appModels/models.py
+++ appModels/models.py
@@ -1,3 +1,96 @@
 from django.db import models
+from django.utils.text import slugify
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(
+        verbose_name= 'Название',
+        max_length=255,
+        unique=True
+    )
+    slug = models.SlugField(
+        verbose_name='Slug',
+        unique=True,
+        blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+class Product(models.Model):
+    name = models.CharField(
+        verbose_name='Название продукта',
+        max_length=255
+    )
+    slug = models.SlugField(
+        verbose_name='Slug',
+        unique=True,
+        blank=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name='Категория'
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+        null=True
+    )
+    main_image = models.ImageField(
+        verbose_name='Загрузите фото продукта',
+        upload_to='main/'
+    )
+    price = MoneyField(
+        verbose_name='Цена',
+        max_digits=14,
+        decimal_places=2,
+        default_currency = 'RUB'
+    )
+    create_at = models.DateTimeField(
+        verbose_name='Дата создания',
+        auto_now_add=True
+    )
+    update_at = models.DateTimeField(
+        verbose_name='Дата изменения',
+        auto_now=True
+    )
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
+    
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='images',
+        verbose_name='Продукт'
+    )
+    image = models.ImageField(
+        upload_to='extra/',
+        verbose_name='Изображение'        
+    )
+
+    def __str__(self):
+        return f"Изображение для {self.product.name}"
+
+    class Meta:
+        verbose_name = "Изображение продукта"
+        verbose_name_plural = "Изображения продуктов"
